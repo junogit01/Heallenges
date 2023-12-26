8c47b9eeaa809a461b3dc9e5b48998ea992253ed
kth <ddsdl@naver.com>
2023-12-26 14:19:17 +0900

    feat : 미션 추가

--- server/routes/mission.js
+++ server/routes/mission.js
@@ -27,22 +27,22 @@ router.get('/:type', (req, res, next) => {
 });
 
 // 미션 상세 조회
-router.get('/:id', function (req, res, next) {
+router.get('/:id', (req, res, next) => {
   const params = req.params; // { id: 5 }
   missionDAO.missionDetail(params, (resp) => {
     res.json(resp);
   });
 });
 
-// 미션 참여
-// router.post('/:id/participate', (req, res, next) => {
-//   const missionId = req.params.id;
-//   const userId = req.user.id;
-
-//   missionDAO.participateMission({ user_id: userId, mission_id: missionId }, (resp) => {
-//     res.json(resp);
-//   });
-// });
+//미션 참여
+router.post('/:id/participate', (req, res, next) => {
+  const missionId = req.params.id;
+  const userId = req.body.user_id;
+  console.log(req.body);
+  missionDAO.participateMission({ user_id: userId, mission_id: missionId }, (resp) => {
+    res.json(resp);
+  });
+});
 
 // 댓글 생성
 router.post('/comments/:id', async (req, res, next) => {
@@ -53,6 +53,13 @@ router.post('/comments/:id', async (req, res, next) => {
 });
 
 // 댓글 수정
+router.put('/comments/:id', async (req, res) => {
+  const data = req.body;
+  data.id = req.params.id;
+  missionDAO.updateComment(data, (resp) => {
+    res.json(resp);
+  });
+});
 
 // 댓글 삭제
 router.delete('/comments/:id', async (req, res, next) => {
