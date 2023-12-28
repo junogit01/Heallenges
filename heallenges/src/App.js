029531653c232eadb5e29fec386746cab2462627
이준호 <esens0607@gmail.com>
2023-12-28 09:43:43 +0900

    feat:Login, Signup, Mypage

--- heallenges/src/App.js
+++ heallenges/src/App.js
@@ -1,11 +1,17 @@
-import Home from "@pages/Home";
-import Layout from "@pages/Layout";
-import { Route, Routes } from "react-router-dom";
+import Home from '@pages/Home';
+import Layout from '@pages/Layout';
+import Mypage from '@pages/Mypage';
+import Login from '@pages/Login';
+import {Route, Routes} from 'react-router-dom';
+import Signup from '@pages/Signup';
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Route>
     </Routes>
   );
---
+++ heallenges/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import LoginHeader from '@components/Login/LoginHead';
+import LoginBody from '@components/Login/LoginBody';
+
+function Login() {
+  return (
+    <>
+      <LoginHeader />
+      <LoginBody />
+    </>
+  );
+}
+
+export default Login;
