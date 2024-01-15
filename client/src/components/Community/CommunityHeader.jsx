bc0713e7ceed67142a2b10e9b2eccc858d383b4d
송형기 <domon2909@naver.com>
2024-01-15 10:39:40 +0900

    chore:merge

--- client/src/components/Community/CommunityHeader.jsx
+++ client/src/components/Community/CommunityHeader.jsx
@@ -40,7 +40,7 @@ function CommunityHeader() {
     //   </div>
     // </nav>
 
-    <div className="navbar navbar-light bg-light" style={{ height: '10.5rem' }}>
+    <div className="navbar navbar-light" style={{ height: '10.5rem' }}>
       <div className="container position-relative d-flex flex-column align-items-center">
         {/* 페이지 제목 */}
         <h2>커뮤니티</h2>
