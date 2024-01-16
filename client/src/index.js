14934382d6380f1d8e87b6c05468e99b1031a7fb
이준호 <esens0607@gmail.com>
2024-01-16 09:57:25 +0900

    feat: index basename feat

--- client/src/index.js
+++ client/src/index.js
@@ -26,7 +26,7 @@ axios.defaults.baseURL = 'http://localhost:8001/api';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="http://heallenges.cafe24app.com/">
       <RecoilRoot>
         <App />
       </RecoilRoot>
